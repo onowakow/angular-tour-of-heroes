ce411ea4281880fde00afc48e056226487df0f3a
onowakow <nowakowskiowen08@gmail.com>
2022-02-13 16:57:56 -0600

    Created dashboard and getting routing running

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-
+  heroes: Hero[] = [];
   // selectedHero optionally takes a Hero interface, but is not initially assigned.
   selectedHero?: Hero;
 
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  getHeroes() : void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
-
 }
