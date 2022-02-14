b263be9b6f80312fb1d649984e906ed3f34b78e1
onowakow <nowakowskiowen08@gmail.com>
2022-02-14 09:40:34 -0600

    Added error handling

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,6 +28,13 @@ export class HeroDetailComponent implements OnInit {
     this.location.back()
   }
 
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'))
     this.heroService.getHero(id)
